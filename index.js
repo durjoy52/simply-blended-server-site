1f28fdc99f3f50b718894e83fde3c0dcbb97c767
durjoy sarkar <durjoywebdeveloper777@gmail.com>
2022-05-11 05:21:58 +0600

    update done

--- index.js
+++ index.js
@@ -1,69 +1,89 @@
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
-const express = require('express');
-const cors = require('cors');
-const app = express()
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { parse } = require("dotenv");
+const { json } = require("express/lib/response");
+const { query } = require("express");
+const app = express();
 const port = process.env.PORT || 5000;
 
-// middleware 
-app.use(cors())
-app.use(express.json())
-
+// middleware
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sctdy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run(){
-    try{
-        await client.connect()
-        const userCollection = client.db('simply-blended').collection('juice')
+async function run() {
+  try {
+    await client.connect();
+    const userCollection = client.db("simply-blended").collection("juice");
 
-        app.get('/products',async(req,res)=>{
-            const cursor = userCollection.find()
-            const result = await cursor.toArray()
-            if(!result?.length){
-                res.send({success:false,error:'No product found'})
-            }
-            res.send({success:true,data:result})
-        })
+    app.get("/products", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      if (!result?.length) {
+        res.send({ success: false, error: "No product found" });
+      }
+      res.send({ success: true, data: result });
+    });
 
-        app.get('/products/:id',async(req,res)=>{
-            const id = req.params.id;
-            console.log(id)
-            const query = {_id: ObjectId(id)}
-            const service = await userCollection.findOne(query)
-            if(!service){
-                res.send({success:false,error:'No product found'})
-            }
-            res.send({success:true,data:service})
-        })
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await userCollection.findOne(query);
+      if (!service) {
+        res.send({ success: false, error: "No product found" });
+      }
+      res.send({ success: true, data: service });
+    });
 
-        // post 
-        app.post('/products',async(req,res)=>{
-            const query = req.body
-            const result = await userCollection.insertOne(query)
-            res.send(result)
-        })
+    // post
+    app.post("/products", async (req, res) => {
+      const query = req.body;
+      const result = await userCollection.insertOne(query);
+      res.send(result);
+    });
 
-        // delete 
-        app.delete('/product/:id',async(req,res)=>{
-            const id = req.params.id;
-            const query ={_id: ObjectId(id)}
+    // delete
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
 
-            const result = await userCollection.deleteOne(query)
+      const result = await userCollection.deleteOne(query);
 
-            res.send(result)
-        })
-    }finally{
+      res.send(result);
+    });
 
-    }
+    // update
+    app.put("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateData = req.body;
+      console.log(updateData)
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          quantity: updateData.quantity
+        },
+      };
+      const result =await userCollection.updateOne(filter,updateDoc,options)
+      res.send(result);
+    });
+  } finally {
+  }
 }
-run().catch(console.dir)
+run().catch(console.dir);
 
-app.get('/',(req,res)=>{
-    res.send('hello how are you?')
-})
+app.get("/", (req, res) => {
+  res.send("hello how are you?");
+});
 
-app.listen(port,()=>{
-    console.log('your port running now',port)
-})
+app.listen(port, () => {
+  console.log("your port running now", port);
+});
